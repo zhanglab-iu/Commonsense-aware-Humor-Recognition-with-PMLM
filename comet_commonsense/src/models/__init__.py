3bc1104ade727bcc87b7c4d789a95d0eaed60901
yuta0306 <yuta.20010306@gmail.com>
2024-02-23 22:25:24 +0900

    rm

--- comet_commonsense/src/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import gpt, models, utils
-
-__all__ = ["gpt", "models", "utils"]
--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .keyword_base import KWComHumor
-from .lightning_model import LightningModel
-from .lm_base import LMBaseModel
-
-__all__ = ["KWComHumor", "LightningModel", "LMBaseModel"]
